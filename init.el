2a375a4d8a642306c7dfa802b44242da4dd9ffed
Morphed Human <163374239+curiosity-led-me-here@users.noreply.github.com>
2026-08-03 02:24:14 +0530

    Initial commit

---
+++ init.el
@@ -0,0 +1,41 @@
+
+(load-theme 'modus-vivendi t)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-startup-screen t)
+
+(set-face-attribute 'default nil
+                    :family "Menlo"
+                    :height 150)
+
+;; Remove GUI clutter
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Start maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Dark built-in theme
+(load-theme 'modus-vivendi t)
+
+;; Font
+(set-face-attribute 'default nil
+                    :family "Menlo"
+                    :height 160)
+
+;; Cleaner appearance
+(setq inhibit-startup-screen t)
+(setq-default cursor-type 'bar)
+(setq visible-bell t)
+
+;; Show line numbers only in programming files
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; Highlight matching parentheses
+(show-paren-mode 1)
+
+;; Remember recently opened files
+(recentf-mode 1)
